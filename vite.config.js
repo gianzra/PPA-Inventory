b8dfd323e8cced5654e794c0b7a7f6ca481cc6ab
shoraa <Gianzra1@gmail.com>
2024-06-29 13:35:28 +0700

    edit1 vite

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
-  build: {
-    outDir: 'public/dist', // Konfigurasi output direktori sesuai dengan Laravel
-    manifest: true, // Generate manifest.json
-    rollupOptions: {
-      input: ['resources/js/app.js'],
-    },
-  },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
